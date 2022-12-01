c8b9191cf427aa082999bbe350f1910570aea85f
J.Alberto Azevedo <alazib7@gmail.com>
2022-12-01 12:49:26 +0100

    8th-iteration-TestRenamed

---
+++ src/__tests__/Exercises.test.js
@@ -0,0 +1,49 @@
+import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import { BrowserRouter } from "react-router-dom"
+import App from "../App"
+
+function renderApp() {
+  return render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  )
+}
+
+describe("Exercises form", () => {
+  test("should render a checkbox with two input: bench press, and dead lift", async () => {
+    renderApp()
+    const nextButton = screen.getByRole("button", { name: /next/i })
+    userEvent.click(nextButton)
+
+    const benchPressInput = await screen.findByRole("checkbox", {
+      name: /bench press/i,
+    })
+    const deadLiftInput = await screen.findByRole("checkbox", {
+      name: /dead lift/i,
+    })
+    expect(benchPressInput).toBeInTheDocument()
+    expect(deadLiftInput).toBeInTheDocument()
+  })
+
+  test("should render two buttons: back and next", () => {
+    renderApp()
+    const nextButton = screen.getByRole("button", { name: /next/i })
+    const backButton = screen.getByRole("button", { name: /back/i })
+
+    expect(backButton).toBeInTheDocument()
+    expect(nextButton).toBeInTheDocument()
+  })
+
+  test("click on next button should render the summary", async () => {
+    renderApp()
+    const nextButton = screen.getByRole("button", { name: /next/i })
+    userEvent.click(nextButton)
+
+    const summaryTitle = await screen.findByRole("heading", {
+      name: /summary/i,
+    })
+    expect(summaryTitle).toBeInTheDocument()
+  })
+})
